93f24bcf67a72596140e02ac8257c6ffc74f7bd0
Emre-Urun <emrern268@gmail.com>
2026-02-02 23:05:18 +0300

    Reset project – start from scratch

--- src/redux/balance/selectors.js
+++
@@ -1,3 +0,0 @@
-export const selectBalance = (state) => state.balance.amount; 
-export const selectBalanceLoading = (state) => state.balance.isLoading;
-export const selectBalanceError = (state) => state.balance.error;
--- src/redux/transactions/selectors.js
+++
@@ -1,3 +0,0 @@
-export const selectTransactions = (state) => state.transactions.items; 
-export const selectTransactionsLoading = (state) => state.transactions.isLoading;
-export const selectTransactionsError = (state) => state.transactions.error;
