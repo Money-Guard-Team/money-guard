eb9ca83f4c409d60a01cd66bc5235e3564cec73c
Ömer ÖZDEMİR <ozd.omer17@gmail.com>
2026-02-04 19:11:58 +0300

    feat: add statistics dashboard components (#31)

---
+++ src/routes/PublicRoute.jsx
@@ -0,0 +1,8 @@
+import { Navigate } from "react-router-dom";
+import { selectIsLoggedIn } from "../redux/auth/selectors";
+import { useSelector } from "react-redux";
+function PublicRoute({ children }) {
+const isLoggedIn = useSelector(selectIsLoggedIn);
+return isLoggedIn ? <Navigate to="/" /> : children;
+}
+export default PublicRoute;
